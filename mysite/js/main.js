3f596ef3431d8843546da4bde810ce00a772ccd6
Denis <cryptomining1988@gmail.com>
2020-04-03 04:26:28 +0300

    Commit mySite

---
+++ mysite/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    $(".slider__inner").slick({
+        /* https://kenwheeler.github.io/slick/ */
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        /* Отключение бесконечной прокрутки слайда  */
+        infinite: false
+    });
+    /*  Стилизация селектов */
+
+
+
+});
